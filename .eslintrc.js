4b970f0300606acb082b72f0424f907003d9b425
Zetaphor <zetaphor@gmail.com>
2020-06-22 20:17:02 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "parserOptions": {
+        "ecmaVersion": 2018,
+    },
+    "extends": "standard",
+    "rules": {
+        "semi": [2, "always"]
+    },
+    "env": {
+        "browser": true
+    }
+};
